6c7504a1f0fc3bfb797299a655f529bd6f17b754
Gergely <gergely06@gmail.com>
2016-12-02 10:48:34 +0100

    .

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ import sys, os
 version = '0.0'
 
 setup(name='myhack',
-      version=version,
+      version=0.1,
       description="trying",
-      long_description="""\
-trying""",
+      long_description="trying",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='myhack',
       author='minus79',
       author_email='gergely06@gmail.com',
-      url='',
-      license='',
+      url='https://github.com/minus79/xenon',
+      download_url = 'https://github.com/minus79/xenon/tarball/0.1',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
